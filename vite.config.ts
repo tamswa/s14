4e15241acb051fda27a5741c6e4ab1f1ce0e5820
Tamswa <85555631+tamswa@users.noreply.github.com>
2026-06-25 04:53:30 +0300

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,10 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: './', //
+  // هذا السطر يخبر Vite أن يضع المسارات بالنسبة للمجلد الحالي
+  base: './', 
+  build: {
+    // التأكد من أن المخرجات تذهب لمجلد dist
+    outDir: 'dist',
+  }
 })
